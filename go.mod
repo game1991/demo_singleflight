ca0ede89234860fad5e4a36f4d7145b03973ca53
Luckystar※ <game1991@github.com>
2024-02-25 21:32:41 +0800

    updat

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module demo
 
 go 1.20
 
-require golang.org/x/sync v0.6.0
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.6.0
+	github.com/hashicorp/golang-lru v1.0.2
+	golang.org/x/sync v0.6.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
+	"strconv"
 	"time"
+
+	"log"
 
 	"demo/utils"
 
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/sync/singleflight"
 )
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 设置日志输出到本地文件
+	log.SetOutput(openLogFile())
+}
+
+func openLogFile() *os.File {
+	f, err := os.OpenFile("demo.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	return f
+}
 
 type Server struct {
 	WorkerOpts *Options
@@ -17,7 +40,9 @@ type Server struct {
 }
 
 type Options struct {
-	GSF *singleflight.Group
+	Cache    *redis.Client
+	LRUCache *lru.Cache
+	GSF      *singleflight.Group
 }
 
 type Worker struct {
@@ -30,9 +55,98 @@ func (s *Server) NewWorker(i int) *Worker {
 	}
 }
 
+type LoggerHook struct {
+	logger *log.Logger
+}
+
+func GetCaller(depth int) string {
+	callers := ""
+	for i := 0; true; i++ {
+		_, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		callers = callers + fmt.Sprintf("%v:%v\n", file, line)
+		if i == depth {
+			break
+		}
+	}
+	return callers
+}
+
+// 请将LoogerHook实现redis.Hook
+func (l *LoggerHook) BeforeProcess(ctx context.Context, cmd redis.Cmder) (context.Context, error) {
+	return ctx, nil
+}
+
+func (l *LoggerHook) AfterProcess(ctx context.Context, cmd redis.Cmder) error {
+	l.logger.Printf("%s cmd: %v\n", GetCaller(2), cmd)
+	return nil
+}
+
+func (l *LoggerHook) BeforeProcessPipeline(ctx context.Context, cmds []redis.Cmder) (context.Context, error) {
+	return ctx, nil
+}
+
+func (l *LoggerHook) AfterProcessPipeline(ctx context.Context, cmds []redis.Cmder) error {
+	l.logger.Printf("%s cmds: %v\n", GetCaller(2), cmds)
+	return nil
+}
+
+func NewReidsClient() *redis.Client {
+	// 初始化redis
+	reidsCli := redis.NewClient(&redis.Options{
+		// 设置 Redis 服务器的地址
+		Addr: "127.0.0.1:6379",
+		// 设置密码
+		Password: "foobared",
+		// 将要使用的数据库设置为默认数据库
+		DB: 0,
+
+		// 设置最大重试次数
+		MaxRetries: 3,
+		// 设置在重试失败操作之前等待的最短时间
+		MinRetryBackoff: 8 * time.Millisecond,
+		// 设置在重试失败操作之前等待的最长时间
+		MaxRetryBackoff: 512 * time.Millisecond,
+
+		// 设置等待建立连接的最长时间
+		DialTimeout: 5 * time.Second,
+		// 设置等待读操作完成的最长时间
+		ReadTimeout: 3 * time.Second,
+		// 设置等待写操作完成的最长时间
+		WriteTimeout: 3 * time.Second,
+
+		// 设置连接池中的最大连接数
+		PoolSize: 10,
+		// 设置从连接池获取连接的最长等待时间
+		PoolTimeout: 30 * time.Second,
+		// 设置连接池中的最小空闲连接数
+		MinIdleConns: 10,
+
+		// 设置连接在连接池中的最长空闲时间
+		IdleTimeout: 60 * time.Second,
+
+		// 设置连接的最长生命周期
+		MaxConnAge: 20 * time.Minute,
+	})
+	// 初始化logger
+	logger := log.New(openLogFile(), "", log.LstdFlags)
+
+	reidsCli.AddHook(&LoggerHook{logger: logger})
+	return reidsCli
+}
+
 func newWorkerOpts() *Options {
+	redisCli := NewReidsClient()
+	lruCache, err := lru.New(100)
+	if err != nil {
+		panic(err)
+	}
 	return &Options{
-		GSF: &singleflight.Group{},
+		Cache:    redisCli,
+		LRUCache: lruCache,
+		GSF:      &singleflight.Group{},
 	}
 }
 
@@ -59,61 +173,83 @@ func main() {
 			<-server.pool
 		}
 	}()
-
+	log.Println("服务启动中...")
 	server.Start(quit)
 }
 
 func (s *Server) Start(quit chan os.Signal) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	for {
 		select {
 		case s := <-quit:
-			fmt.Println("退出信号", s)
+			log.Println("退出信号", s)
 			return
 		case id := <-s.pool:
-			s.Poll(id)
+			s.Poll(ctx, id)
 		}
 	}
 }
 
-func (s *Server) Poll(id int) {
+func (s *Server) Poll(ctx context.Context, id int) {
 	worker := s.NewWorker(id)
 
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
 				callers := utils.GetCallers(err)
-				fmt.Printf("[poller] worker run panic: %q, Call stack:%s\n", err, callers)
+				log.Printf("[poller] worker run panic: %q, Call stack:%s\n", err, callers)
 			}
 			s.pool <- id
 		}()
-
-		worker.Do(id)
+		key := strconv.Itoa(int(time.Now().Unix())) // 以当前时刻作为key，精度到秒
+		worker.Do(ctx, id, key)
 	}()
-
-	worker.Do(id)
 }
 
-func (w *Worker) Do(id int) {
-	fmt.Println("worker", id, "doing")
+func (w *Worker) Do(ctx context.Context, id int, key string) {
+	log.Printf("worker[%d] doning...[key]=%s\n", id, key)
+	// 先从本地lru缓存中读取
+	data, ok := w.LRUCache.Get(key)
+	if ok {
+		log.Printf("worker[%d] get lru cache result: %v\n", id, data)
+		return
+	}
+
+	data, err := w.Cache.Get(ctx, key).Result()
+	if err != nil && err != redis.Nil && !errors.Is(err, redis.Nil) {
+		panic("获取缓存失败" + err.Error())
+	}
+	if err == nil {
+		log.Printf("worker[%d] get cache result: %v\n", id, data)
+		return
+	}
 
 	// 由于业务需要调用外部服务，所以使用singleflight，避免重复调用
-	result := w.GSF.DoChan("key", func() (interface{}, error) {
+	result := w.GSF.DoChan(key, func() (interface{}, error) {
 		// mock some external service call
 		time.Sleep(2 * time.Second)
 		if id == 13 {
 			return nil, fmt.Errorf("mock service call error")
 		}
+		data := "data" + uuid.New().String()
 
+		// 存入本地缓存
+		w.LRUCache.Add(key, data)
 
+		_, err := w.Cache.SetEX(ctx, key, data, time.Minute).Result()
+		if err != nil {
+			return nil, fmt.Errorf("set cache error: %w", err)
+		}
 
-		return nil, nil
+		return data, nil
 	})
 
 	res := <-result
 	if res.Err != nil {
-		fmt.Println("worker", id, "error")
+		log.Println("worker", id, "error")
 		return
 	}
 
-	fmt.Printf("worker %d result: %v\n", id, res.Val)
+	log.Printf("worker %d result: %v\n", id, res.Val)
 }
